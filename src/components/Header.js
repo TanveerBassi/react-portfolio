346bc943fb71897a3adb0cb640f06eb34e1dce0b
Tanveer Bassi <tanveer13a551@gmail.com>
2022-08-22 23:57:45 +0100

    images and some pages sorted

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Navigation from "./Navigation";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function Header(props) {
+  return (
+    <Navbar
+      collapseOnSelect
+      className="navbar"
+      variant="dark"
+      expand="sm"
+      aria-current="true"
+    >
+      <Navbar.Brand href="/portfolio-React" className="name">
+        {" "}
+        &lt; Tanveer /&gt;
+      </Navbar.Brand>
+      {/* Responsive Navbars */}
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      {/* wrap your Navs in a Navbar.Collapse component.  */}
+      <Navbar.Collapse
+        id="responsive-navbar-nav"
+        className=" justify-content-end"
+      >
+        <Navigation setCurrentPage={props.setCurrentPage}></Navigation>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
---
+++ src/components/Navigation.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+
+export default function Navigation(props) {
+  const { setCurrentPage } = props;
+
+  return (
+    <Nav className="pages">
+      {/* -------------------About page------------------- */}
+      <Nav.Link
+        className="page"
+        href="#about"
+        onClick={() => {
+          setCurrentPage("About");
+        }}
+      >
+        About
+      </Nav.Link>
+
+      {/* -------------------Portfolio page------------------- */}
+      <Nav.Link
+        className="page"
+        href="#portfolio"
+        onClick={() => {
+          setCurrentPage("Portfolio");
+        }}
+      >
+        Projects
+      </Nav.Link>
+
+      {/* -------------------Contact page------------------- */}
+      <Nav.Link
+        className="page"
+        href="#contact"
+        onClick={() => {
+          setCurrentPage("Contact");
+        }}
+      >
+        Contact
+      </Nav.Link>
+
+      {/* -------------------Resume page------------------- */}
+      <Nav.Link
+        className="page"
+        href="#resume"
+        onClick={() => {
+          setCurrentPage("Resume");
+        }}
+      >
+        Resume
+      </Nav.Link>
+    </Nav>
+  );
+}
+
+// it needs to be an arrow function inside the onClick()
+// the title corresponding to the current section is highlighted
