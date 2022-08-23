a26be59aa313e7937b52235834d31963bb5df69b
Tanveer Bassi <tanveer13a551@gmail.com>
2022-08-23 10:48:21 +0100

    Home page sorted issue with package to be resolved

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Typed from "react-typed";
+import webIcon from "../../assets/icons/web.png";
+import Particles from "react-tsparticles";
+import { loadLinksPreset } from "tsparticles-preset-links";
+import Footer from "../Footer";
+
+function particlesInit(tsParticles) {
+  console.log("init", tsParticles);
+
+  loadLinksPreset(tsParticles);
+}
+
+export default function Home() {
+  return (
+    <div>
+      <div className="container">
+        <div className="intro">
+          <h1 className="hi">Hi, I am Tanveer Bassi</h1>
+          <Typed
+            className="typing"
+            strings={["Full Stack Developer", "Finance Professional"]}
+            typeSpeed={80}
+            backSpeed={30}
+            loop
+          />
+          <div>
+            <img src={webIcon} className="webicon" alt="web development icon" />{" "}
+          </div>
+        </div>
+
+        <Particles
+          options={{
+            preset: "links",
+          }}
+          init={particlesInit}
+        />
+      </div>
+      <Footer />
+    </div>
+  );
+}
