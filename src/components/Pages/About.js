b70122f9ee46a8ac21fed207ffd507865cb0b676
Tanveer Bassi <tanveer13a551@gmail.com>
2022-08-24 11:58:28 +0100

    About me bio updated

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -14,40 +14,25 @@ export default function About() {
         <Col md={7} lg={8} className="profile-div">
           <div className="description">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              I'm a full stack web developer, confident in working with a MERN
+              stack. I also have experience using other frameworks such as
+              GraphQl and jQuery, relational databases with MySQL, and of course
+              writing in Vanilla JavaScript.
             </p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              I'm currently a finance professional looking to make the career
+              switch to tech. I'm currently working as a mortgage adviser and
+              have previously worked a multitude of jobs in the finance sector
+              from financial adviser to complaint handler.
             </p>
+
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              I take great pride in not only being able to complete work to the
+              high standard you expect but also go the extra mile wherever
+              possible. I enjoy getting to know a business, its goals, and how I
+              can best apply myself to make sure your website succeeds. I
+              particularly like analysing business metrics and making data-based
+              decisions and recommendations, leading to better rates of success.
             </p>
           </div>
         </Col>
