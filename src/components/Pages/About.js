77407f78bdf8d9389b82d66a31044e8ebda5162b
TanveerBassi <Tanveer13a551@gmail.com>
2024-07-01 21:27:47 +0100

    updated image

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import myImage from "../../assets/images/T.png";
+import myImage from "../../assets/images/T.jpg";
 
 export default function About() {
   return (
