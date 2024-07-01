26cc61d2764ea2679fef66303717341fc24a4115
TanveerBassi <Tanveer13a551@gmail.com>
2024-07-01 14:59:00 +0100

    updated particles

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 // import Typed from "react-typed";
 import webIcon from "../../assets/icons/web.png";
 import Footer from "../Footer";
-// import Particles from "react-tsparticles";
-// import { loadFull } from "tsparticles";
-// import { useCallback } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+
+import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 
 export default function Home() {
-  // const particlesInit = useCallback(async (engine) => {
-  //   console.log(engine);
-  //   await loadFull(engine);
-  // }, []);
+  const [init, setInit] = useState(false);
 
-  // const particlesLoaded = useCallback(async (container) => {
-  //   await console.log(container);
-  // }, []);
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadFull(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
     <div>
       <div className="container">
@@ -31,83 +43,82 @@ export default function Home() {
             <img src={webIcon} className="webicon" alt="web development icon" />{" "}
           </div>
         </div>
-        {/* <Particles
-          id="tsparticles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={{
-            background: {
-              color: {
-                value: "000000",
+
+        {init && (
+          <Particles
+            id="tsparticles"
+            particlesLoaded={particlesLoaded}
+            options={{
+              background: {
+                color: {
+                  value: "#000000",
+                },
               },
-            },
-            fpsLimit: 120,
-            interactivity: {
-              events: {
-                onClick: {
+              fpsLimit: 120,
+              interactivity: {
+                events: {
+                  onClick: {
+                    enable: true,
+                    mode: "push",
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: "repulse",
+                  },
+                  resize: true,
+                },
+                modes: {
+                  push: {
+                    quantity: 4,
+                  },
+                  repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                  },
+                },
+              },
+              particles: {
+                color: {
+                  value: "#00c853",
+                },
+                links: {
+                  color: "#00c853",
+                  distance: 150,
                   enable: true,
-                  mode: "push",
+                  opacity: 0.5,
+                  width: 1,
                 },
-                onHover: {
+                move: {
+                  direction: "none",
                   enable: true,
-                  mode: "repulse",
+                  outModes: {
+                    default: "bounce",
+                  },
+                  random: false,
+                  speed: 6,
+                  straight: false,
                 },
-                resize: true,
-              },
-              modes: {
-                push: {
-                  quantity: 4,
+                number: {
+                  density: {
+                    enable: true,
+                    area: 800,
+                  },
+                  value: 180,
                 },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4,
+                opacity: {
+                  value: 0.5,
+                },
+                shape: {
+                  type: "circle",
+                },
+                size: {
+                  value: { min: 1, max: 5 },
                 },
               },
-            },
-            particles: {
-              color: {
-                value: "00c853",
-              },
-              links: {
-                color: "#b2ff59",
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 1,
-              },
-              collisions: {
-                enable: true,
-              },
-              move: {
-                directions: "none",
-                enable: true,
-                outModes: {
-                  default: "bounce",
-                },
-                random: false,
-                speed: 6,
-                straight: false,
-              },
-              number: {
-                density: {
-                  enable: true,
-                  area: 800,
-                },
-                value: 80,
-              },
-              opacity: {
-                value: 0.5,
-              },
-              shape: {
-                type: "circle",
-              },
-              size: {
-                value: { min: 1, max: 5 },
-              },
-            },
-            detectRetina: true,
-          }}
-        /> */}
+              detectRetina: true,
+            }}
+          />
+        )}
       </div>
       <Footer />
     </div>
